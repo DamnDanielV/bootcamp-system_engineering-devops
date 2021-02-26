5e7d4b8d0d567ced084eb68c4a29ba2b02f79028
DamnDanielV <1821@holbertonschool.com>
2021-02-25 19:29:42 -0500

    addfed tasks

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# comment - to pass checker
+exec { 'sed':
+  command => "/bin/sed -Ei  sed -Ei '/holberton/s/[0-9]+/20/g' /etc/security/limits.conf",
+}
+
+exec { 'restart':
+  command => '/usr/sbin/service nginx restart',
+}
